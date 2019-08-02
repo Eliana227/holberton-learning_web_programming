da0183041e07f898bd40912f6ddd04a43b759798
Eliana227 <53445619+Eliana227@users.noreply.github.com>
2019-08-02 14:55:58 -0400

    Update 3-my_first_loop.js

--- 5-javascript/3-my_first_loop.js
+++ 5-javascript/3-my_first_loop.js
@@ -3,15 +3,3 @@ while (i <= 10){
     console.log(i);
     i++;
 }
-
-
-for (var i=1;i<=100;i++) {console.log(i);
-}
-
-function double(number){
-    return number*2}
-function word (Hello World){
-        return 5/9 * (Hello World)}
-
-
-var names of dwarves from Snow White = ["Bashful", "Sleepy", "Snezzy"];
